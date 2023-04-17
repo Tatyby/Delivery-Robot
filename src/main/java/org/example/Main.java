95c1e0c1fd0df7573652ab321834a4e49364003c
Tatiana <in_sdo@mail.ru>
2023-04-17 15:18:06 +0300

    freqlog

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,29 +6,38 @@ public class Main {
 
     public static int lenght = 100;
     public static String letters = "RLRFR";
-    public static int NumberOfThreads = 1000;
+    public static int numberOfThreads = 10;
     public static String R = "R";
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
     public static void main(String[] args) {
-
-        while (NumberOfThreads > 0) {
+        while (numberOfThreads > 0) {
             new Thread(() -> {
                 String str = generateRoute(letters, lenght);
                 int key = countKey(str, R); //ключ-количество раз буквы R
                 synchronized (sizeToFreq) {
+
                     if (sizeToFreq.containsKey(key)) {
                         sizeToFreq.put(key, sizeToFreq.get(key) + 1);
                     } else {
                         sizeToFreq.put(key, 1);
                     }
+                    sizeToFreq.notify();
                 }
+
             }).start();
-            NumberOfThreads--;
+            numberOfThreads--;
         }
 
-        System.out.println("Самое частое количество повторений " + maxKey(sizeToFreq) +
-                " (встретилось " + maxKeyValue(sizeToFreq) + " раз)");
+
+        MyThread myThread = new MyThread();
+        myThread.start();
+        myThread.interrupt();
+
+
+//
+//        System.out.println("Самое частое количество повторений " + maxKey(sizeToFreq) +
+//                " (встретилось " + maxKeyValue(sizeToFreq) + " раз)");
 
         System.out.println("Другие размеры:");
         sizeToFreq.entrySet().stream()
@@ -55,5 +64,22 @@ public class Main {
 
     public static int maxKeyValue(Map<Integer, Integer> map) {
         return Collections.max(map.entrySet(), Comparator.comparingInt(Map.Entry::getKey)).getValue();
+    }
+
+    public static class MyThread extends Thread {
+        @Override
+        public void run() {
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                System.out.println("Самое частое количество повторений " + maxKey(sizeToFreq) +
+                        " (встретилось " + maxKeyValue(sizeToFreq) + " раз)");
+            }
+        }
     }
 }
